f83369554e08097410caea6fdcc6cf8a0fbfd505
shajardini <sha.jardini@gmail.com>
2023-06-06 15:34:11 -0300

    Atualização

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')//importa p Express
 const mysql = require('mysql2')//import biblioteca mysql2
 
+
+
+
 //configurações do banco de dados
 const dbConfig = {
     host: 'localhost',//host do banco
@@ -22,6 +25,29 @@ connection.connect((err)=>{
     }
     console.log('Conexão estabelecida com sucesso!')
 })
+
+const app = express()
+const cors = require('cors')
+
+app.use(cors())//Adicionar o middleware cors 
+
+//Rota para obter todos os filmes
+app.get('/filme', (req, res) =>{
+    connection.query('SELECT * FROM filmes',(err, resultados)=>{
+        if(err){
+            console.error('Erro ao executar a consulta:', err)
+            res.status(500).json({error: 'Erro ao buscar filmes'})
+        }else{
+            res.json(resultados)
+        }
+    })
+
+})
+
+app.listen(3001, ()=>{
+    console.log('API ESTÁ RODANDO NA PORTA 3001')
+})
+
 
 
 
